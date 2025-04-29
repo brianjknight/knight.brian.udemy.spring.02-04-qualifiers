68fb7c289b5c0bccdc703961722826b48638465c
Brian Knight <knight.j.brian@gmail.com>
2025-04-29 09:07:20 -0600

    completed #46

--- src/main/java/knight/brian/springcoredemo/rest/DemoController.java
+++ src/main/java/knight/brian/springcoredemo/rest/DemoController.java
@@ -2,6 +2,7 @@ package knight.brian.springcoredemo.rest;
 
 import knight.brian.springcoredemo.common.Coach;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,17 +11,17 @@ public class DemoController {
 
     private Coach myCoach;
 
-    // Example of setter injection
-//    @Autowired
-//    public DemoController(Coach theCoach) {
-//        myCoach = theCoach;
-//    }
-
-    // Example of setter injection
+    // Example of constructor injection
     @Autowired
-    public void setCoach(Coach theCoach) { 
+    public DemoController(@Qualifier("cricketCoach") Coach theCoach) {
         myCoach = theCoach;
     }
+
+    // Example of setter injection
+//    @Autowired
+//    public void setCoach(Coach theCoach) {
+//        myCoach = theCoach;
+//    }
 
     @GetMapping("/dailyworkout")
     public String getDailyWorkout() {
